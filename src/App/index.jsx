3a0bf59e07028eee8e590efdc6eba76fb9b3f9f7
Devin Chen <5204106+xxd3vin@users.noreply.github.com>
2022-05-04 16:00:22 +0800

    github page

---
+++ src/App/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { App as DbMan } from "@db-man/components";
+
+import "antd/dist/antd.css";
+
+export default function App() {
+  return (
+    <DbMan />
+  );
+}
