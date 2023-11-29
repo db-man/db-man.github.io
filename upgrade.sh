066d08b2747c215ced3934459ff65e1e4c36a202
Deleted user <5204106+xx7y7xx@users.noreply.github.com>
2023-11-29 14:05:28 +0000

    upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+npm i
+
+git diff
+
+git config --global user.name 'Upgrade Bot'
+git config --global user.email 'db-man-upgrade-bot@users.noreply.github.com'
+# git commit -am "npm i"
+git diff --quiet && git diff --staged --quiet || git commit -am 'npm i'
+git push
