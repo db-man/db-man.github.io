7d07c8224b8fc7ece70b8e31e853b08cc7e97d5c
Deleted user <5204106+xx7y7xx@users.noreply.github.com>
2024-02-21 17:07:58 +0000

    commit msg

--- upgrade.sh
+++ upgrade.sh
@@ -7,5 +7,5 @@ git diff
 git config --global user.name 'Upgrade Bot'
 git config --global user.email 'db-man-upgrade-bot@users.noreply.github.com'
 # git commit -am "npm i"
-git diff --quiet && git diff --staged --quiet || git commit -am 'npm i'
+git diff --quiet && git diff --staged --quiet || git commit -am '[CI] [upgrade.sh] npm i'
 git push
