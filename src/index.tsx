dc992ea4611bf8567cedb6a021f9b57f56eeceab
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-10-30 09:12:55 +0800

    refactor: Move App component to DbMan directory and update imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App/index';
+import App from './DbMan';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
